45360fce5487ed5eeed38859bb602bc5ce260900
Jason Smythe <jason@smythemail.za.net>
2017-04-13 17:54:10 +0200

    I really need to be more careful...

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
-var People = artifacts.require("./SimpleStorage.sol");
+var SimpleStorage = artifacts.require("./SimpleStorage.sol")
+
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(SimpleStorage)
 };
